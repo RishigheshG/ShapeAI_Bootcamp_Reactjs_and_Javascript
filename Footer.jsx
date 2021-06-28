1857c68ebc86de893cfb2c3af258584bdc8a0897
XDawnBreakerX <46450828+XDawnBreakerX@users.noreply.github.com>
2021-06-28 12:46:54 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header>
+      <footer>
+        <p>Copyright by Rishighesh @ {new Date().getFullYear()}</p>
+      </footer>
+    </header>
+  );
+}
+
+export default Footer;
